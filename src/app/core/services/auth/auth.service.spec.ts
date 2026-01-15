fc1c64f63e71d2188e0e1ea10e24b4861a79059f
Juno Lupus <junoplupus@gmail.com>
2026-01-15 12:01:33 -0300

    Implementação de Testes unitários (#15)
    Testes realizados:
    
    - Estado do Usuário (`MapToDomain`): Deve transformar corretamente o User do Firebase no User da aplicação (mapeando `uid`, `email`, etc) quando o observable `user()` emitir.
    
    - Tratamento de Nulo: O observable `currentUser$` deve emitir `null` caso o Firebase emita `null` (usuário deslogado).
    
    - Login (Email/Senha): Deve chamar `signInWithEmailAndPassword` com os parâmetros corretos.
    
    - Login (Google): Deve instanciar o `GoogleAuthProvider` e chamar `signInWithPopup` com os parâmetros corretos.
    
    - [x] Logout: Deve chamar `signOut`.
    
    - Verificar se os erros de login são propagados para AuthService.
    
    - Obter ID Seguro: O método `getValidUserId` deve retornar o `ID` se logado, ou lançar exceção se deslogado (importante para os testes do Facade!).

--- src/app/core/services/auth/auth.service.spec.ts
+++ src/app/core/services/auth/auth.service.spec.ts
@@ -1,16 +1,151 @@
 import { TestBed } from '@angular/core/testing';
+import { BehaviorSubject } from 'rxjs';
 
 import { AuthService } from './auth.service';
 
+const mockAuthState$ = new BehaviorSubject<any>(null);
+
+jest.mock('@angular/fire/auth', () => {
+
+  return {
+    user: () => mockAuthState$.asObservable(),
+
+    signInWithEmailAndPassword: jest.fn(),
+    signInWithPopup: jest.fn(),
+    signOut: jest.fn(),
+
+    GoogleAuthProvider: jest.fn(),
+
+    getAuth: jest.fn(),
+    Auth: jest.fn()
+  }
+});
+
+import {
+  Auth,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  GoogleAuthProvider
+} from '@angular/fire/auth';
+
+
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService : AuthService;
+  let authMock : any;
+
+  const firebaseUserMock = {
+    uid: '1',
+    email: 'test@gmail.com',
+    displayName: 'Test User',
+    photoURL: null,
+    password: '123'
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthService);
+
+    authMock = {
+      currentUser: firebaseUserMock
+    }
+
+    TestBed.configureTestingModule({
+      providers: [
+        AuthService,
+        { provide: Auth, useValue: authMock }
+      ]
+    });
+
+    authService = TestBed.inject(AuthService);
+
+    mockAuthState$.next(null);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(authService).toBeTruthy();
+  });
+
+  it('should transform FirebaseUser to domain User entity', (done) => {
+    mockAuthState$.next(firebaseUserMock);
+
+    authService.currentUser$.subscribe(userDomain => {
+
+      expect(userDomain).toBeTruthy();
+
+      expect(userDomain?.uid).toBe(firebaseUserMock.uid);
+      expect(userDomain?.email).toBe(firebaseUserMock.email);
+
+      expect(userDomain?.levelInfo).toBeDefined();
+      expect(userDomain?.levelConfiguration).toBeDefined();
+
+      done();
+    });
+  });
+
+  it('should handle with null FirebaseUser (not authenticated)', (done) => {
+    authService.currentUser$.subscribe(userDomain => {
+
+      expect(userDomain).toBeNull();
+
+      done();
+    });
+  });
+
+  it('should login with email and password', async () => {
+    (signInWithEmailAndPassword as jest.Mock).mockReturnValue(Promise.resolve(undefined));
+
+    await authService.loginWithEmailAndPassword(
+      authMock.currentUser.email,
+      authMock.currentUser.password
+    );
+
+    expect(signInWithEmailAndPassword).toHaveBeenCalledWith(
+      authMock,
+      authMock.currentUser.email,
+      authMock.currentUser.password
+    );
+  });
+
+  it('should login with google', async () => {
+    (signInWithPopup as jest.Mock).mockReturnValue(Promise.resolve(undefined));
+
+    await authService.loginWithGoogle();
+
+    expect(GoogleAuthProvider).toHaveBeenCalled();
+    expect(signInWithPopup).toHaveBeenCalledWith(authMock, expect.any(GoogleAuthProvider));
+  });
+
+  it('should propagate error when login fails', async () => {
+
+    const firebaseError = new Error('auth/wrong-password');
+    (signInWithEmailAndPassword as jest.Mock).mockReturnValue(Promise.reject(firebaseError));
+
+    await expect(authService.loginWithEmailAndPassword('email','password'))
+      .rejects.toThrow('auth/wrong-password');
+  });
+
+  it('should logout', async () => {
+    (signOut as jest.Mock).mockReturnValue(Promise.resolve());
+
+    await authService.logout();
+
+    expect(signOut).toHaveBeenCalledWith(authMock);
+  });
+
+  it('should return id if user is authenticated', () => {
+
+    const userId = authService.getUserId();
+
+    expect(userId).toBe(firebaseUserMock.uid);
+  });
+
+  it('should throw error if user is not authenticated', () => {
+    authMock.currentUser = null;
+
+    expect(() => authService.getValidUserId())
+      .toThrow('Usuário não autenticado.')
   });
 });
